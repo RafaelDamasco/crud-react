27791ec150ac14aba58c92ca87493a05fc7b9baf
Rafael Damasco <rafaeldamasco_@hotmail.com>
2024-03-15 15:46:14 -0300

    feat: add SignUp page and form validation

---
+++ src/pages/auth/sign-up.tsx
@@ -0,0 +1,89 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { zodResolver } from '@hookform/resolvers/zod'
+import z from 'zod'
+
+const signUpFormSchema = z.object({
+  name: z.string().min(1),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type SignUpFormType = z.infer<typeof signUpFormSchema>
+
+export function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<SignUpFormType>({
+    resolver: zodResolver(signUpFormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  })
+  function handleSignUp(data: SignUpFormType) {
+    try {
+      console.log(data)
+    } catch (error) {
+      console.log(errors)
+    }
+  }
+  return (
+    <>
+      <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-in">Login</Link>
+        </Button>
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <h1 className="text-2xl font-semibold tracking-tight text-center">
+            Create account
+          </h1>
+
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignUp)}>
+            <div className="space-y-2">
+              <Label htmlFor="name">Your name</Label>
+              <Input id="name" type="text" {...register('name')} />
+              {errors.name && (
+                <p className="text-red-500">{errors.name.message}</p>
+              )}
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Your e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+              {errors.email && (
+                <p className="text-red-500">{errors.email.message}</p>
+              )}
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Your password</Label>
+              <Input id="password" type="password" {...register('password')} />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+            </div>
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
+              Finish Sign Up
+            </Button>
+            <p className="text-m leading-rtext-muted-foreground px-6 text-center text-sm">
+              By continuing you agree to our{' '}
+              <a className="underline underline-offset-4" href="">
+                terms of service{' '}
+              </a>
+              and{' '}
+              <a className="underline underline-offset-4" href="">
+                privacy policies
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+      <h1>SignUp</h1>
+    </>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,16 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/home/home'
 import { SignIn } from './pages/auth/sign-in'
 import { AuthLayout } from './layout/auth'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   { path: '/', element: <Home /> },
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/login', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
 ])
