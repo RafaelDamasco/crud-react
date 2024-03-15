6e2a9223104cb8ef0802af1027c285cfad18a8b5
Rafael Damasco <rafaeldamasco_@hotmail.com>
2024-03-15 14:52:14 -0300

    chore: add tailwindcss, shadcn and resolve alias paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import './global.css'
 
 export function App() {
   return (
     <div>
       <RouterProvider router={router} />
-      <h1>Hello world</h1>
     </div>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Home } from './pages/home/home'
 import { SignIn } from './pages/auth/sign-in'
+import { AuthLayout } from './layout/auth'
 
 export const router = createBrowserRouter([
   { path: '/', element: <Home /> },
-  { path: '/login', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/login', element: <SignIn /> }],
+  },
 ])
