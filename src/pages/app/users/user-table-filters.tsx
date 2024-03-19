c6aa986b0107c606931bf5bb1e9917131235e284
Rafael Damasco <rafaeldamasco_@hotmail.com>
2024-03-19 17:07:08 -0300

    feat: add UserTableFilters component

---
+++ src/pages/app/users/user-table-filters.tsx
@@ -0,0 +1,51 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { UsersContext } from '@/contexts/userContext'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Search } from 'lucide-react'
+import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
+
+export function UserTableFilters() {
+  const { getUsers } = useContext(UsersContext)
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+  async function handleSearchUsers(data: SearchFormInputs) {
+    await getUsers(data.query)
+  }
+  return (
+    <form
+      onSubmit={handleSubmit(handleSearchUsers)}
+      className="flex items-center gap-2"
+    >
+      <span className="text-sm font-semibold">Filters:</span>
+      <Input
+        placeholder="User name"
+        className="h-8 w-[320px]"
+        {...register('query')}
+      />
+
+      <Button
+        type="submit"
+        variant="secondary"
+        size="xs"
+        disabled={isSubmitting}
+      >
+        <Search className="mr-2 size-4" />
+        Filter results
+      </Button>
+    </form>
+  )
+}
